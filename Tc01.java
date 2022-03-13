8c17fceb50113130c5cf342d90b7deecb919c848
Prasad Patil <100512265+Prasadpatil2408@users.noreply.github.com>
2022-03-13 16:53:10 +0530

    "tc_01 added"

---
+++ Tc01.java
@@ -0,0 +1,8 @@
+public static void main(String[]args){
+		String s= "This is java program";
+		System.out.println(s.length());
+		for(int i=s.length()-1;i>=0;i--){
+		System.out.print(s.charAt(i));
+		}
+		}	}
+	
